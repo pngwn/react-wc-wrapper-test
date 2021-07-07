a25dd42358183aedb9d34b94a01769768aa1188c
pngwn <hello@pngwn.io>
2021-07-08 00:56:21 +0100

    get styles working

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { App } from './App';
-import { helper } from './wc-helper';
+import { helper, helper_two} from './wc-helper';
 
 
 const wc = helper(App);
+const wc_two = helper_two(App);
+
 
 customElements.define('react-test', wc)
+customElements.define('react-test-two', wc_two)
--- src/wc-helper.jsx
+++ src/wc-helper.jsx
@@ -1,14 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
+
+const styles = `div {
+	float: left;
+	display: block;
+	border:1px solid #ccc;
+	border-radius: 3px;
+	width: 500px;
+	text-align: center;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+`
+
+function getCSS(url, mount) {
+	return new Promise((res, rej) => {
+		const link = document.createElement('link');
+		link.onload = res;
+		link.onerror = rej;
+		link.href = `${url}/assets/App.css`;
+		link.rel = 'stylesheet';
+		mount.appendChild(link);
+	});
+}
+
 export function helper(Component) {
 
 	// create web component class
+
 	return class WC extends HTMLElement {
 		connectedCallback() {
-			const mountPoint = document.createElement('span');
-			this.attachShadow({ mode: 'open' }).appendChild(mountPoint);
+			const mountPoint = document.createElement('div');
+			let style = document.createElement('style');
+			style.textContent = styles;
 
+			const root = this.attachShadow({ mode: 'closed' });
+			root.appendChild(style);
+			root.appendChild(mountPoint);
+
+			getCSS('http://localhost:3000', root).then(_ => {
+				ReactDOM.render(<Component />, mountPoint);
+			})
+		}
+	}
+}
+
+export function helper_two(Component) {
+
+	// create web component class
+
+	return class WC extends HTMLElement {
+		connectedCallback() {
+			const mountPoint = document.createElement('div');
+			let style = document.createElement('style');
+			style.textContent = styles;
+
+			const root = this.attachShadow({ mode: 'closed' });
+			root.appendChild(style);
+			root.appendChild(mountPoint);
 
 			ReactDOM.render(<Component />, mountPoint);
 		}
